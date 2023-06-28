f8e29314264eff6fa73d4e5113772bfb095fcffd
ygoswami1204 <ygoswami@terpmail.umd.edu>
2023-06-27 21:55:02 -0400

    6/27cooked

--- 2-Setup Sims and Calcs/Simulators/SlipAngleSim.m
+++ 2-Setup Sims and Calcs/Simulators/SlipAngleSim.m
@@ -1,24 +1,29 @@
 %% Slip Angle Simulator
 
-function [SlipAngles,AccelG,Betamax,YawVelo,LateralVelo] = SlipAngleSim(SteerAngles,Beta,Velocity,Accel,Radius,vehicle)
+function [SlipAngles,AccelG,Betamax,YawVelo,LongVelo,LateralVelo] = SlipAngleSim(SteerAngles,Beta,Velocity,Radius,vehicle)
     % Velocity Calculations
-    Velocityin_s = Velocity * 17.6; %in/s
+    LongVelo = Velocity * 17.6; %in/s
     % Accel = -Velocityin_s.^2/Radius; %in/s^2
-    AccelG = -Accel/386.4; %g's
-    YawVelo = -Accel/Velocityin_s; %rad/s
-    LateralVelo = Velocityin_s*(Beta*(pi/180)); %(in-rad)/s
+    LateralVelo = -LongVelo*tand(Beta); %in/s
+    YawVelo = (LongVelo/cosd(Beta))/-Radius; %rad/s
+    AccelG = ((LongVelo/cosd(Beta))*YawVelo)/386.4; %g's
     
     % CoG Slip Angle Calculations
-    Betamax = (-AccelG/AccelG)*(atan(vehicle.CoGToRearAxle/Radius))*(180/pi); %deg
+    Betamax = (atan(vehicle.CoGToRearAxle/-Radius))*(180/pi); %deg
     % neg -> Right, pos -> Left
     
     % Steer Angle Conversion (rad)
     SteerAnglesrad = deg2rad(-SteerAngles);
     
     % Slip Angle Calculations
-    SlipAnglesF = [((((LateralVelo + (YawVelo*vehicle.FrontAxleToCoG))/(Velocityin_s-(YawVelo*(vehicle.FrontTrackWidth/2))))-SteerAnglesrad(1,1))),((((LateralVelo + (YawVelo*vehicle.FrontAxleToCoG))/(Velocityin_s+(YawVelo*(vehicle.FrontTrackWidth/2))))-SteerAnglesrad(1,2)))];
-    SlipAnglesR = [((((LateralVelo - (YawVelo*vehicle.CoGToRearAxle))/(Velocityin_s-(YawVelo*(vehicle.RearTrackWidth/2)))))-SteerAnglesrad(2,1)),((((LateralVelo - (YawVelo*vehicle.CoGToRearAxle))/(Velocityin_s+(YawVelo*(vehicle.RearTrackWidth/2))))-SteerAnglesrad(2,2)))];
+    SlipAnglesF = [((((LateralVelo + (YawVelo*vehicle.FrontAxleToCoG))/(LongVelo-(YawVelo*(vehicle.FrontTrackWidth/2))))-SteerAnglesrad(1,1))),((((LateralVelo + (YawVelo*vehicle.FrontAxleToCoG))/(LongVelo+(YawVelo*(vehicle.FrontTrackWidth/2))))-SteerAnglesrad(1,2)))];
+    SlipAnglesR = [((((LateralVelo - (YawVelo*vehicle.CoGToRearAxle))/(LongVelo-(YawVelo*(vehicle.RearTrackWidth/2)))))-SteerAnglesrad(2,1)),((((LateralVelo - (YawVelo*vehicle.CoGToRearAxle))/(LongVelo+(YawVelo*(vehicle.RearTrackWidth/2))))-SteerAnglesrad(2,2)))];
             
+    if(Beta == 0)
+        SlipAnglesF = [-SteerAnglesrad(1,1),-SteerAnglesrad(1,2)];
+        SlipAnglesR = [-SteerAnglesrad(2,1),-SteerAnglesrad(2,2)];
+    end
+    
     SlipAngles = [SlipAnglesF; SlipAnglesR]*(180/pi);
     
     if(Velocity == 0)
@@ -26,4 +31,5 @@ function [SlipAngles,AccelG,Betamax,YawVelo,LateralVelo] = SlipAngleSim(SteerAng
         Betamax = 0;
         YawVelo = 0;
     end
+    
 end
--- 2-Setup Sims and Calcs/Simulators/YMSim.m
+++ 2-Setup Sims and Calcs/Simulators/YMSim.m
@@ -3,19 +3,20 @@
 function [YM,Accel] = YMSim(SteerAngles,Fx,Fy,Mz,vehicle)
     
     SteerAnglesrad = -SteerAngles*(pi/180);
-    car_Fx = Fx.*cos(SteerAnglesrad) + Fy.*sin(SteerAnglesrad); %tire forces in car coordinate system
+    car_Fx = Fx.*cos(SteerAnglesrad) - Fy.*sin(SteerAnglesrad); %tire forces in car coordinate system
     car_Fy = Fx.*sin(SteerAnglesrad) + Fy.*cos(SteerAnglesrad); %tire forces in car coordinate system
     car_totalFx = sum(sum(car_Fx));
     car_totalFy = sum(sum(car_Fy));
     
-    % Moment_armsx = [a, a; -b, -b];
-    % Moment_armsy = [TrackWidth(1,:)/2,-TrackWidth(1,:)/2; TrackWidth(2,:)/2,-TrackWidth(2,:)/2]/12; %ft
+    % Yaw Moment (lb*in)
+    Moment_armsx = [vehicle.FrontAxleToCoG, vehicle.FrontAxleToCoG; -vehicle.CoGToRearAxle, -vehicle.CoGToRearAxle]; %in
+    Moment_armsy = [vehicle.FrontTrackWidth/2,-vehicle.FrontTrackWidth/2; vehicle.RearTrackWidth/2,-vehicle.RearTrackWidth/2]; %in
     
-    % car_totalMz = sum(car_Fy.*Moment_armsx + car_Fx.*Moment_armsy) + sum(Mz);
+    YM = sum(sum(car_Fy.*Moment_armsx + car_Fx.*Moment_armsy) + sum(Mz));
     
     % Yaw Moment (lb*in)
-    YM = ((Fx(1,1)*(vehicle.FrontTrackWidth/24))+(Fx(2,1)*(vehicle.RearTrackWidth/24)))-((Fx(1,2)*(vehicle.FrontTrackWidth/24))+(Fx(2,2)*(vehicle.RearTrackWidth/24)))+ ((Fy(1,1)+Fy(1,2))*vehicle.FrontAxleToCoG)-((Fy(2,1)+Fy(2,2))*vehicle.CoGToRearAxle)-Mz(1,1)-Mz(1,2)-Mz(2,1)-Mz(2,2);
+    % YM = ((Fx(1,1)*(vehicle.FrontTrackWidth/24))+(Fx(2,1)*(vehicle.RearTrackWidth/24)))-((Fx(1,2)*(vehicle.FrontTrackWidth/24))+(Fx(2,2)*(vehicle.RearTrackWidth/24)))+ ((Fy(1,1)+Fy(1,2))*vehicle.FrontAxleToCoG)-((Fy(2,1)+Fy(2,2))*vehicle.CoGToRearAxle)-Mz(1,1)-Mz(1,2)-Mz(2,1)-Mz(2,2);
     
     % Acceleration (g's)
-    Accel = ([car_totalFx car_totalFy]/vehicle.TotalWeight)/32.2;
+    Accel = ([car_totalFx car_totalFy]/vehicle.TotalWeight);
 end
