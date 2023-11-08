a1ecf39ecdff83fce34545b538051aa89d682296
ygoswami1204 <ygoswami@terpmail.umd.edu>
2023-11-08 00:59:25 -0500

    Update YMD_Test.m

--- 2-Setup Sims and Calcs/Simulators/YMD_Test.m
+++ 2-Setup Sims and Calcs/Simulators/YMD_Test.m
@@ -27,116 +27,143 @@ vehicleObj = TREV2Parameters();
 
 %% Tire Modeling
 
-%Input tire filenames
-filename.FrontTire = 'A1965run18.mat';
-filename.RearTire = 'A1965run18.mat';
-[trainingDataFront,tire.IDfront] = createLatTrngData2(filename.FrontTire);
-[trainingDataRear,tire.IDrear] = createLatTrngData2(filename.RearTire);
+% Input Front and Rear Tire Data
+% Front
+filename_P1F = 'A2356run8.mat';
+[latTrainingData_P1F,tire.IDF,test.IDF] = createLatTrngDataCalc(filename_P1F);
+
+filename_P2F = 'A2356run9.mat';
+[latTrainingData_P2F,tire.IDF,test.IDF] = createLatTrngDataCalc(filename_P2F);
+
+totDataF = cat(1,latTrainingData_P1F,latTrainingData_P2F);
+trainDataF = totDataF;
+
+% Rear
+filename_P1R = 'A2356run8.mat';
+[latTrainingData_P1R,tire.IDR,test.IDR] = createLatTrngDataCalc(filename_P1R);
+
+filename_P2R = 'A2356run9.mat';
+[latTrainingData_P2R,tire.IDR,test.IDR] = createLatTrngDataCalc(filename_P2R);
+
+totDataR = cat(1,latTrainingData_P1R,latTrainingData_P2R);
+trainDataR = totDataR;
 
 % Front tires
-disp([tire.IDfront, ', Front Tire Model is being trained.  Standby...'])
+disp([tire.IDF, ', Front Tire Model is being trained.  Standby...'])
 t1 = tic;
-[model.FxFront, validationRMSE.FxFront] = Trainer_Fx(trainingDataFront);
-[model.FyFront, validationRMSE.FyFront] = Trainer_Fy(trainingDataFront);
-[model.MzFront, validationRMSE.MzFront] = Trainer_Mz(trainingDataFront);
-[model.muxFront, validation.RMSE_muxFront] = Trainer_mux(trainingDataFront);
-[model.muyFront, validation.RMSE_muyFront] = Trainer_muy(trainingDataFront);
+[model.FxFront, validationRMSE.FxFront] = Trainer_Fx(trainDataF);
+[model.FyFront, validationRMSE.FyFront] = Trainer_Fy(trainDataF);
+[model.MzFront, validationRMSE.MzFront] = Trainer_Mz(trainDataF);
+% [model.muxFront, validation.RMSE_muxFront] = Trainer_mux(trainDataF);
+% [model.muyFront, validation.RMSE_muyFront] = Trainer_muy(trainDataF);
 toc(t1)
 
 disp('Training completed')
 
 % Rear tires
-disp([tire.IDrear, ', Rear Tire Model is being trained.  Standby...'])
+disp([tire.IDR, ', Rear Tire Model is being trained.  Standby...'])
 t1 = tic;
-[model.FxRear, validationRMSE.FxRear] = Trainer_Fx(trainingDataRear);
-[model.FyRear, validationRMSE.FyRear] = Trainer_Fy(trainingDataRear);
-[model.MzRear, validationRMSE.MzRear] = Trainer_Mz(trainingDataRear);
-[model.muxRear, validation.RMSE_muxRear] = Trainer_mux(trainingDataRear);
-[model.muyRear, validation.RMSE_muyRear] = Trainer_muy(trainingDataRear);
+[model.FxRear, validationRMSE.FxRear] = Trainer_Fx(trainDataR);
+[model.FyRear, validationRMSE.FyRear] = Trainer_Fy(trainDataR);
+[model.MzRear, validationRMSE.MzRear] = Trainer_Mz(trainDataR);
+% [model.muxRear, validation.RMSE_muxRear] = Trainer_mux(trainDataR);
+% [model.muyRear, validation.RMSE_muyRear] = Trainer_muy(trainDataR);
 toc(t1)
 
 disp('Training completed')
 
-%% Inputs
+%% Tuned Car Parameters
 
-K_t = [548 548; 548 548];%lbf/in 
+% Tire Spring Rates (lbf/in)
+[F_polyCalc_Kt,R_polyCalc_Kt] = SpringRateCalc(latTrainingData_P1F,latTrainingData_P2F,vehicleObj);
 
-% Number of Iterations
-m = 19;
-n = 13;
-
-% Input Test Cornering Parameters
-Radius = linspace(-1000,1000,10); %-348; %in (neg -> Left, pos -> Right)
-Velocity = 25; %linspace(0,35,10); %mph
-
-% Input Steering Wheel Angle, CoG Slip Angle
-SWAngle = linspace(-90,90,m); %deg (L = neg, R = pos)
-Beta = 6; %linspace(6,-6,n); %CoG slip angle (deg) (pos -> Right, neg -> Left)
-
-%% Code
+K_t = [F_polyCalc_Kt, F_polyCalc_Kt; R_polyCalc_Kt, R_polyCalc_Kt];
 
 % Stiffnesses (lbf/in)
-[K_w,K_r,K_roll] = StiffnessSim(K_t,vehicleObj);
+[K_w,K_r,K_roll] = StiffnessCalc(K_t,vehicleObj);
 
-% Entry Analysis
-Accel = [0 0];
+[F_polyCalc,R_polyCalc] = LateralCoFCalc(latTrainingData_P1F,latTrainingData_P2F,vehicleObj);
 
-% Steering Angles (deg), Slip Angles (deg), Load Transfer (lb), Wheel Displacement (in) (neg -> loaded (bump), pos -> unloaded (droop))
-for k = 1:length(Beta)
-    for j = 1:length(SWAngle)
-        for i = 1:length(Radius)
-                           
-            SteerAngles(:,:,j) = SteerAngleCalc(SWAngle(:,j),vehicleObj);
-            
-            [SlipAngles(:,:,i),LatAccelG(:,:,i),Betamax(:,:,i),YawVelo(:,:,i),LongVelo(:,:,i),LateralVelo(:,:,i)] = SlipAngleCalc(SteerAngles(:,:,j),Beta(:,k),Velocity,Radius(:,i),vehicleObj);
-            
-            [Fz(:,:,i),LLT(:,:,i),LLT_D(:,:,i),R_g(:,:,i),Roll_Angle(:,:,i),Z(:,:,i)] = LLTSim(K_roll,Velocity,Accel(:,2),vehicleObj);
-            
-            [IA(:,:,i)] = CamberSim(Roll_Angle(:,:,i),SWAngle,vehicleObj);
-            
-            [Fx(:,:,i),Fy(:,:,i),Mz(:,:,i)] = findTireFM(model,SlipAngles(:,:,i),IA(:,:,i),Fz(:,:,i),vehicleObj.TirePressure);
-            
-            if(Velocity == 0)
-                Fx = [0 0; 0 0];
-                Fy = [0 0; 0 0];
-                Mz = [0 0; 0 0];
-            end
-            
-            [YM(:,:,i),Accel(:,:,i)] = YMSim(SteerAngles(:,:,j),Fx(:,:,i),Fy(:,:,i),Mz(:,:,i),vehicleObj);
-            
-            disp('Radius: ');
-            disp(Radius(:,i));
-            disp('Steering Wheel Angle: ');
-            disp(SWAngle(:,j));
-%             disp('Beta: ');
-%             disp(Beta(:,k));
-%             disp('Roll Angle: ');
-%             disp(Roll_Angle(:,:,i));
-%             disp('Slip Angles: ');
-%             disp(SlipAngles(:,:,i));
-%             disp('Fx: ');
-%             disp(Fx(:,:,i));
-%             disp('Fy: ');
-%             disp(Fy(:,:,i));
-%             disp('Fz: ');
-%             disp(Fz(:,:,i));
-%             disp('Mz: ');
-%             disp(Mz(:,:,i));
-            disp('Gs: ');
-            disp(Accel(:,:,i));
-%             disp('Acceleration: ');
-%             disp(LatAccelG(:,:,i));
-            disp('Yaw Moment: ');
-            disp(YM(:,:,i));
-%             disp('Camber: ');
-%             disp(IA(:,:,i));
-%             disp('Wheel Displacement: ');
-%             disp(Z(:,:,i));
-%             disp('Tire Pressure: ');
-%             disp(vehicleObj.TirePressure);
-            disp('----------------------');
-       
-        end
+%% Motor Parameters
+
+Max_Velocity = 86; % mph
+
+%% Inputs
+
+VelocityInput = 0; % mph
+
+SWAngle = -2; % deg (pos->Right, neg->Left)
+
+BetaInput = 1; % deg (pos->Right, neg->Left)
+
+Radius = 329; % in (pos->Right, neg->Left)
+
+converge = false;
+
+%% Calculations
+
+while(converge == false)
+
+    [SteerAngles,TurnRadius] = SteerAngleCalc(SWAngle,vehicleObj);
+    
+    [SlipAngles] = SlipAngleCalc(SteerAngles,BetaInput,VelocityInput,Radius,vehicleObj);
+    
+    Accelcalc = -((VelocityInput*17.6)^2/Radius)/386.4; % g's
+    
+    [Fz,LLT,LLT_D,R_g,Roll_Angle,Z] = LLTCalc(K_r,K_roll,VelocityInput,Accelcalc,vehicleObj);
+                
+    [IA] = CamberCalc(Roll_Angle,SWAngle,vehicleObj);
+                
+    [Fx,Fy,Mz] = findTireFM(model,SlipAngles,IA,Fz,vehicleObj.TirePressure);
+    
+    [YM,Accel] = YMCalc(SteerAngles,Fx,Fy,Mz,vehicleObj);
+
+    % Acceltot = sqrt(Accel(1,1)^2 + Accel(1,2)^2);
+    
+    % NextBeta = atand(vehicleObj.CoGToRearAxle/Radius) - (((-2*vehicleObj.RearStatic)/sum(Calpha(2,:))*(((Velocity*17.6)^2)/(Radius*386.4))));
+    % Vcalc = sqrt(abs((Acceltot*386.4))*Radius)./17.6;
+
+    if (abs(Accelcalc - Accel(1,2))>(0.0001*abs(Accelcalc)))
+        Vcalc = sqrt(abs((Accel(1,2)*386.4))*Radius)./17.6;
+        VelocityInput = Vcalc;
+    else
+        converge = true;
     end
+
 end
 
+disp('Velocity: ');
+disp(Vcalc);
+disp('Radius: ');
+disp(Radius);
+disp('Steering Wheel Angle: ');
+disp(SWAngle);
+disp('Input Beta: ');
+disp(BetaInput);
+% disp('Next Beta: ');
+% disp(NextBeta);
+disp('Roll Angle: ');
+disp(Roll_Angle);
+disp('Slip Angles: ');
+disp(SlipAngles);
+disp('Fx: ');
+disp(Fx);
+disp('Fy: ');
+disp(Fy);
+disp('Fz: ');
+disp(Fz);
+disp('Mz: ');
+disp(Mz);
+disp('Gs: ');
+disp(Accel);
+% disp('Acceleration: ');
+% disp(LatAccelG);
+disp('Yaw Moment: ');
+disp(YM);
+disp('Camber: ');
+disp(IA);
+disp('Wheel Displacement: ');
+disp(Z);
+disp('Tire Pressure: ');
+disp(vehicleObj.TirePressure);
+disp('----------------------');
