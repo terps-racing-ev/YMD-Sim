9b8051b84f06490494f0f5c8febe8ec7339e2860
Floatingtugboat <127701342+Floatingtugboat@users.noreply.github.com>
2024-03-20 01:17:53 -0400

    changed steerangle calcs to convert something to inches. Changed YMD_Test to base convergence on velocity
    I would probably change the YMD_Test to work on change of accel instead of velocity. Still getting  an issue with calc negativve Yaw Moment. Graph doesn't look right

--- 2-Setup Sims and Calcs/Calculators/YMCalc.m
+++ 2-Setup Sims and Calcs/Calculators/YMCalc.m
@@ -5,8 +5,8 @@ function [YM,Accel] = YMCalc(SteerAngles,Fx,Fy,Mz,vehicle)
     SteerAnglesrad = SteerAngles*(pi/180);
     car_Fx = Fx.*cos(SteerAnglesrad) - Fy.*sin(SteerAnglesrad); %tire forces in car coordinate system
     car_Fy = Fx.*sin(SteerAnglesrad) + Fy.*cos(SteerAnglesrad); %tire forces in car coordinate system
-    car_totalFx = sum(sum(car_Fx));
-    car_totalFy = sum(sum(car_Fy));
+    car_totalFx = sum(sum(car_Fx))
+    car_totalFy = sum(sum(car_Fy))
     
     % Yaw Moment (lb*in)
     Moment_armsx = [vehicle.FrontAxleToCoG, vehicle.FrontAxleToCoG; -vehicle.CoGToRearAxle, -vehicle.CoGToRearAxle]; %in
@@ -18,5 +18,5 @@ function [YM,Accel] = YMCalc(SteerAngles,Fx,Fy,Mz,vehicle)
     % YM = ((Fx(1,1)*(vehicle.FrontTrackWidth/24))+(Fx(2,1)*(vehicle.RearTrackWidth/24)))-((Fx(1,2)*(vehicle.FrontTrackWidth/24))+(Fx(2,2)*(vehicle.RearTrackWidth/24)))+ ((Fy(1,1)+Fy(1,2))*vehicle.FrontAxleToCoG)-((Fy(2,1)+Fy(2,2))*vehicle.CoGToRearAxle)-Mz(1,1)-Mz(1,2)-Mz(2,1)-Mz(2,2);
     
     % Acceleration (g's)
-    Accel = ([car_totalFx -car_totalFy]/vehicle.TotalWeight);
+    Accel = ([car_totalFx -car_totalFy]/vehicle.TotalWeight)
 end
